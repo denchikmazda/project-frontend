a01c1dfa16370f05c0f3c104ac7dd3198ae5dd3a
denchik <danil-efimov2000mag@mail.ru>
2023-06-05 16:36:31 +0300

    add styles, theme, helper func classNames

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useContext, useState } from "react";
 import { Link, Route, Routes } from "react-router-dom";
-import "./index.scss";
+import "./styles/index.scss";
 import { AboutPageAsync } from "./Pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./Pages/MainPage/MainPage.async";
+import { Theme, ThemeContext } from "./theme/ThemeContext";
+import { useTheme } from "./theme/useTheme";
+import { classNames } from "./helpers/classNames/classNames";
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className="app">
+    <div className={classNames("app", {}, [theme])}>
+      <button onClick={toggleTheme}>TOGGLE</button>
       <Link to={"/"}>Главная </Link>
       <Link to={"/about"}>О сайте</Link>
       <Suspense fallback={<div>Loading...</div>}>
