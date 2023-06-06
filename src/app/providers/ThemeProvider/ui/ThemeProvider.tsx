5abc81242bb91d2a4ea013ceeafcbf3b6c7db442
denchik <danil-efimov2000mag@mail.ru>
2023-06-06 15:42:47 +0300

    edit structure, add AppRouter, Navbar, UI Kit Link

--- src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
+++ src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -1,5 +1,9 @@
 import React, { FC, useMemo, useState } from "react";
-import { ThemeContext, LOCAL_STORAGE_THEME_KEY, Theme } from "./ThemeContext";
+import {
+  ThemeContext,
+  LOCAL_STORAGE_THEME_KEY,
+  Theme,
+} from "../lib/ThemeContext";
 
 const defaultTheme =
   (localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme) || Theme.LIGHT;
