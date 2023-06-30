62b0452fb7ce7d90a0ae2904f7b1f4a335addc36
denchik <danil-efimov2000mag@mail.ru>
2023-06-30 04:11:23 +0300

    fix tests

--- src/shared/lib/tests/renderWithTranslation/renderWithTranslation.tsx
+++ src/shared/lib/tests/renderWithTranslation/renderWithTranslation.tsx
@@ -1,12 +1,27 @@
-import { render } from '@testing-library/react';
 import { ReactNode } from 'react';
+import { render } from '@testing-library/react';
 import { I18nextProvider } from 'react-i18next';
 import i18nForTests from 'shared/config/i18n/i18nForTests';
+import { MemoryRouter } from 'react-router-dom';
 
-export function renderWithTranslation(component: ReactNode) {
+export interface renderWithTranslationOptions {
+    route?: string;
+}
+
+export function renderWithTranslation(
+    component: ReactNode,
+    options: renderWithTranslationOptions = {},
+) {
+    const {
+        route = '/',
+    } = options;
+
     return render(
-        <I18nextProvider i18n={i18nForTests}>
-            {component}
-        </I18nextProvider>,
+        <MemoryRouter initialEntries={[route]}>
+            <I18nextProvider i18n={i18nForTests}>
+                {component}
+            </I18nextProvider>
+            ,
+        </MemoryRouter>,
     );
 }
