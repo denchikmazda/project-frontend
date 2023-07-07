7cac38529c87d6f3c2c364623aadb7178f4bdedb
denchik <danil-efimov2000mag@mail.ru>
2023-07-07 13:35:30 +0300

    fix translates in storybook

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -1,8 +1,29 @@
+// import { addDecorator } from '@storybook/react';
+// import { Theme } from '../../src/app/providers/ThemeProvider';
+// import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator';
+// import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator';
+// import { RouterDecorator } from '../../src/shared/config/storybook/RouterDecorator';
+
+// export const parameters = {
+//     actions: { argTypesRegex: '^on[A-Z].*' },
+//     controls: {
+//         matchers: {
+//             color: /(background|color)$/i,
+//             date: /Date$/,
+//         },
+//     },
+// };
+
+// addDecorator(StyleDecorator);
+// addDecorator(RouterDecorator);
+// addDecorator(ThemeDecorator(Theme.LIGHT));
+
 import { addDecorator } from '@storybook/react';
 import { Theme } from '../../src/app/providers/ThemeProvider';
 import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator';
 import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator';
 import { RouterDecorator } from '../../src/shared/config/storybook/RouterDecorator';
+import i18n from './i18n';
 
 export const parameters = {
     actions: { argTypesRegex: '^on[A-Z].*' },
@@ -11,6 +32,19 @@ export const parameters = {
             color: /(background|color)$/i,
             date: /Date$/,
         },
+    },
+    themes: {
+        default: 'light',
+        list: [
+            { name: 'light', class: ['app', 'light'], color: '#FFF' },
+            { name: 'dark', class: ['app', 'dark'], color: '#2d3748' },
+        ],
+    },
+    i18n,
+    locale: 'en',
+    locales: {
+        en: 'English',
+        ru: 'Russian',
     },
 };
 
