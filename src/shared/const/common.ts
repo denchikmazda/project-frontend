2237489b7091c4fe47fe74649080953cfdd2b54f
denchik <danil-efimov2000mag@mail.ru>
2023-07-11 22:20:30 +0300

    fix lint

--- src/shared/const/common.ts
+++ src/shared/const/common.ts
@@ -1,14 +1,14 @@
 export enum Currency {
-    "RUB" = "RUB",
-    "USD" = "USD",
-    "EUR" = "EUR"
+    RUB = 'RUB',
+    USD = 'USD',
+    EUR = 'EUR'
 }
 
 export enum Country {
-    Russia = "Russia",
-    Belarus = "Belarus",
-    Kazakhstan = "Kazakhstan",
-    Urkaine = "Urkaine",
-    Armenia = "Armenia",
-    Georgia = "Georgia"
+    Russia = 'Russia',
+    Belarus = 'Belarus',
+    Kazakhstan = 'Kazakhstan',
+    Urkaine = 'Urkaine',
+    Armenia = 'Armenia',
+    Georgia = 'Georgia'
 }
