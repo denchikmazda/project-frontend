5225004962ba4e9c5a9131ff5ee59cee97b68a03
denchik <danil-efimov2000mag@mail.ru>
2023-08-01 01:03:06 +0300

    fix path name

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,5 +1,5 @@
 import webpack from 'webpack';
-import { buildCssLoader } from './loaders/BuildCssLoader';
+import { buildCssLoader } from './loaders/buildCssLoader';
 import { BuildOptions } from './types/config';
 
 export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
