cf9591f344180858975bb90b7d6188acb15901f7
denchik <danil-efimov2000mag@mail.ru>
2023-08-04 19:28:21 +0300

    add skeleton, code, icon, article details card

--- src/shared/ui/Text/Text.stories.tsx
+++ src/shared/ui/Text/Text.stories.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
-import { Text, TextTheme } from './Text';
+import { Text, TextSize, TextTheme } from './Text';
 
 export default {
     title: 'shared/Text',
@@ -55,3 +55,10 @@ onlyTextDark.args = {
     text: 'Lorem ipsum dolor sit amet sit amet',
 };
 onlyTextDark.decorators = [ThemeDecorator(Theme.DARK)];
+
+export const SizeL = Template.bind({});
+SizeL.args = {
+    title: 'Title Lorem ipsum',
+    text: 'Lorem ipsum dolor sit amet sit amet',
+    size: TextSize.L,
+};
