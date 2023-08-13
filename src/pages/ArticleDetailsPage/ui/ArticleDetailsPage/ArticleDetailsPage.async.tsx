247dc3746b2caa51d6d4544db98a6ef756e75857
denchik <danil-efimov2000mag@mail.ru>
2023-08-14 00:29:32 +0300

    add fetching articles, view type selector

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.async.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.async.tsx
@@ -3,5 +3,5 @@ import { lazy } from 'react';
 export const ArticleDetailsPageAsync = lazy(() => new Promise((resolve) => {
     // Проверить работоспособность
     // @ts-ignore
-    setTimeout(() => resolve(import('./ArticleDetailsPage')), 1000);
+    setTimeout(() => resolve(import('./ArticleDetailsPage')), 400);
 }));
--- src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.async.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.async.tsx
@@ -3,5 +3,5 @@ import { lazy } from 'react';
 export const ArticlesPageAsync = lazy(() => new Promise((resolve) => {
     // Проверить работоспособность
     // @ts-ignore
-    setTimeout(() => resolve(import('./ArticlesPage')), 1500);
+    setTimeout(() => resolve(import('./ArticlesPage')), 400);
 }));
