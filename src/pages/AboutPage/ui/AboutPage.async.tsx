5aa9ac02f83567be0f1177eca7a7501051d21afb
denchik <danil-efimov2000mag@mail.ru>
2023-08-22 14:19:38 +0300

    add copy plugin, netlify production, fix api config

--- src/pages/AboutPage/ui/AboutPage.async.tsx
+++ src/pages/AboutPage/ui/AboutPage.async.tsx
@@ -1,7 +1,3 @@
 import { lazy } from 'react';
 
-export const AboutPageAsync = lazy(() => new Promise((resolve) => {
-    // Проверить работоспособность
-    // @ts-ignore
-    setTimeout(() => resolve(import('./AboutPage')), 1500);
-}));
+export const AboutPageAsync = lazy(() => import('./AboutPage'));
--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.async.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.async.tsx
@@ -1,7 +1,3 @@
 import { lazy } from 'react';
 
-export const ArticleDetailsPageAsync = lazy(() => new Promise((resolve) => {
-    // Проверить работоспособность
-    // @ts-ignore
-    setTimeout(() => resolve(import('./ArticleDetailsPage')), 400);
-}));
+export const ArticleDetailsPageAsync = lazy(() => import('./ArticleDetailsPage'));
