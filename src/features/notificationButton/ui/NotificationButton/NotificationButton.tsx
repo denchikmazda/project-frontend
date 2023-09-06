35315c3bb88ed74c650224b5076411c6817fa9b0
denchik <danil-efimov2000mag@mail.ru>
2023-09-06 12:33:12 +0300

    add drawer, add device-detector (hook useDevice)

--- src/features/notificationButton/ui/NotificationButton/NotificationButton.tsx
+++ src/features/notificationButton/ui/NotificationButton/NotificationButton.tsx
@@ -1,10 +1,13 @@
-import { memo } from 'react';
+import { memo, useCallback, useState } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 import { Icon } from 'shared/ui/Icon/Icon';
 import NotificationIcon from 'shared/assets/icons/notification-20-20.svg';
 import { NotificationList } from 'entities/Notification';
 import { Popover } from 'shared/ui/popups';
+import { Drawer } from 'shared/ui/Drawer/Drawer';
+// import { BrowserView, MobileView } from 'react-device-detect';
+import { useDevice } from 'shared/lib/hooks/useDevice/useDevice';
 import cls from './NotificationButton.module.scss';
 
 interface NotificationButtonProps {
@@ -13,18 +16,59 @@ interface NotificationButtonProps {
 
 export const NotificationButton = memo((props: NotificationButtonProps) => {
     const { className } = props;
+    const isMobile = useDevice();
+    const [isOpen, setIsOpen] = useState(false);
 
-    return (
-        <Popover
-            className={classNames(cls.NotificationButton, {}, [className])}
-            direction="bottom left"
-            trigger={(
-                <Button theme={ButtonTheme.CLEAR}>
-                    <Icon Svg={NotificationIcon} inverted />
-                </Button>
-            )}
-        >
-            <NotificationList className={cls.notifications} />
-        </Popover>
+    const onOpenDrawer = useCallback(() => {
+        setIsOpen(true);
+    }, []);
+
+    const onCloseDrawer = useCallback(() => {
+        setIsOpen(false);
+    }, []);
+
+    const trigger = (
+        <Button onClick={onOpenDrawer} theme={ButtonTheme.CLEAR}>
+            <Icon Svg={NotificationIcon} inverted />
+        </Button>
     );
+
+    return !isMobile
+        ? (
+            <Popover
+                className={classNames(cls.NotificationButton, {}, [className])}
+                direction="bottom left"
+                trigger={trigger}
+            >
+                <NotificationList className={cls.notifications} />
+            </Popover>
+        )
+        : (
+            <>
+                {trigger}
+                <Drawer isOpen={isOpen} onClose={onCloseDrawer}>
+                    <NotificationList />
+                </Drawer>
+            </>
+        );
+    // решение с использование библиотеки device detect
+    // return (
+    //     <div>
+    //         <BrowserView>
+    //             <Popover
+    //                 className={classNames(cls.NotificationButton, {}, [className])}
+    //                 direction="bottom left"
+    //                 trigger={trigger}
+    //             >
+    //                 <NotificationList className={cls.notifications} />
+    //             </Popover>
+    //         </BrowserView>
+    //         <MobileView>
+    //             {trigger}
+    //             <Drawer isOpen={isOpen} onClose={onCloseDrawer}>
+    //                 <NotificationList />
+    //             </Drawer>
+    //         </MobileView>
+    //     </div>
+    // );
 });
