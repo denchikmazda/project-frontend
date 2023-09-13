8b6ae76931451e490e56a55bb6d574690b7b6026
denchik <danil-efimov2000mag@mail.ru>
2023-09-13 21:52:53 +0300

    add helpers for selectors, slice, actions, fix screenshoots

--- src/entities/Counter/ui/Counter.tsx
+++ src/entities/Counter/ui/Counter.tsx
@@ -1,35 +1,46 @@
 import { useTranslation } from 'react-i18next';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
-import { getCounterValue } from '../model/selectors/getCounterValue/getCounterValue';
-import { counterActions } from '../model/slice/counterSlice';
+import { useCounterValue } from '../model/selectors/getCounterValue/getCounterValue';
+import { useCounterActions } from '../model/slice/counterSlice';
 
 import { Button } from '@/shared/ui/Button';
 
 export const Counter = () => {
     const dispatch = useDispatch();
-    const counterValue = useSelector(getCounterValue);
+    const counterValue = useCounterValue();
     const { t } = useTranslation();
+    const { decrement, increment, add } = useCounterActions();
 
-    const increment = () => {
-        dispatch(counterActions.increment());
+    const handleDec = () => {
+        decrement();
     };
 
-    const decrement = () => {
-        dispatch(counterActions.decrement());
+    const handleInc = () => {
+        increment();
+    };
+
+    const handleAdd = () => {
+        add(5);
     };
 
     return (
         <div>
             <h1 data-testid="value-title">{counterValue}</h1>
             <Button
-                onClick={increment}
+                onClick={handleAdd}
+                data-testid="increment-btn5"
+            >
+                {t('add5')}
+            </Button>
+            <Button
+                onClick={handleInc}
                 data-testid="increment-btn"
             >
                 {t('increment')}
             </Button>
             <Button
-                onClick={decrement}
+                onClick={handleDec}
                 data-testid="decrement-btn"
             >
                 {t('decrement')}
