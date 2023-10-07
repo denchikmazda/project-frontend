9200e9a1102d7f91d00f70f463fc5ab2b85b1c32
denchik <danil-efimov2000mag@mail.ru>
2023-10-07 12:45:39 +0300

    redesign list article view, skeleton, outline btn

--- src/entities/Article/ui/ArticleListItem/ArticleListItem.tsx
+++ src/entities/Article/ui/ArticleListItem/ArticleListItem.tsx
@@ -1,124 +1,18 @@
-import { HTMLAttributeAnchorTarget, memo } from 'react';
-import { useTranslation } from 'react-i18next';
+import { memo } from 'react';
 
-import {
-    ArticleBlockType,
-    ArticleView,
-} from '../../model/consts/articleConsts';
-import { Article, ArticleTextBlock } from '../../model/types/article';
-import { ArticleTextBlockComponent } from '../ArticleTextBlockComponent/ArticleTextBlockComponent';
+import { ArticleListItemProps } from '../../model/types/articleListItemProps';
 
-import EyeIcon from '@/shared/assets/icons/eye-20-20.svg';
-import { getRouteArticlesDetails } from '@/shared/const/router';
-import { classNames } from '@/shared/lib/classNames/classNames';
-import { AppLink } from '@/shared/ui/deprecated/AppLink';
-import { Avatar } from '@/shared/ui/deprecated/Avatar';
-import { Button, ButtonTheme } from '@/shared/ui/deprecated/Button';
-import { Card } from '@/shared/ui/deprecated/Card';
-import { Icon } from '@/shared/ui/deprecated/Icon';
-import { Skeleton } from '@/shared/ui/deprecated/Skeleton';
-import { Text } from '@/shared/ui/deprecated/Text';
-import { AppImage } from '@/shared/ui/redesigned/AppImage';
+import { ArticleListItemDeprecated } from './ArticleListItemDeprecated/ArticleListItemDeprecated';
+import { ArticleListItemRedesigned } from './ArticleListItemRedesigned/ArticleListItemRedesigned';
 
-import cls from './ArticleListItem.module.scss';
-
-interface ArticleListItemProps {
-    className?: string;
-    article: Article;
-    view: ArticleView;
-    target?: HTMLAttributeAnchorTarget;
-}
+import { ToggleFeatures } from '@/shared/lib/features';
 
 export const ArticleListItem = memo((props: ArticleListItemProps) => {
-    const { className, article, view, target } = props;
-    const { t } = useTranslation('article');
-
-    const types = <Text text={article.type.join(', ')} className={cls.types} />;
-    const views = (
-        <>
-            <Text text={String(article.views)} className={cls.views} />
-            <Icon Svg={EyeIcon} />
-        </>
-    );
-
-    if (view === ArticleView.LIST) {
-        const textBlock = article.blocks.find(
-            (block) => block.type === ArticleBlockType.TEXT,
-        ) as ArticleTextBlock;
-
-        return (
-            <div
-                className={classNames(cls.ArticleListItem, {}, [
-                    className,
-                    cls[view],
-                ])}
-                data-testid="ArticleListItem"
-            >
-                <Card className={cls.card}>
-                    <div className={cls.header}>
-                        <Avatar size={30} src={article.user.avatar} />
-                        <Text
-                            text={article.user.username}
-                            className={cls.username}
-                        />
-                        <Text text={article.createdAt} className={cls.date} />
-                    </div>
-                    <Text title={article.title} className={cls.title} />
-                    {types}
-                    <AppImage
-                        fallback={<Skeleton width="100%" height={250} />}
-                        src={article.img}
-                        alt={article.title}
-                        className={cls.img}
-                    />
-                    {textBlock && (
-                        <ArticleTextBlockComponent
-                            block={textBlock}
-                            className={cls.textBlock}
-                        />
-                    )}
-                    <div className={cls.footer}>
-                        <AppLink
-                            target={target}
-                            to={getRouteArticlesDetails(article.id)}
-                        >
-                            <Button theme={ButtonTheme.OUTLINE}>
-                                {t('Read more')}
-                            </Button>
-                        </AppLink>
-                        {views}
-                    </div>
-                </Card>
-            </div>
-        );
-    }
-
     return (
-        <AppLink
-            target={target}
-            to={getRouteArticlesDetails(article.id)}
-            className={classNames(cls.ArticleListItem, {}, [
-                className,
-                cls[view],
-            ])}
-            data-testid="ArticleListItem"
-        >
-            <Card className={cls.card}>
-                <div className={cls.imageWrapper}>
-                    <AppImage
-                        fallback={<Skeleton width={200} height={200} />}
-                        alt={article.title}
-                        src={article.img}
-                        className={cls.img}
-                    />
-                    <Text text={article.createdAt} className={cls.date} />
-                </div>
-                <div className={cls.infoWrapper}>
-                    {types}
-                    {views}
-                </div>
-                <Text text={article.title} className={cls.title} />
-            </Card>
-        </AppLink>
+        <ToggleFeatures
+            feature="isAppRedesigned"
+            on={<ArticleListItemRedesigned {...props} />}
+            off={<ArticleListItemDeprecated {...props} />}
+        />
     );
 });
--- src/shared/ui/redesigned/Text/Text.tsx
+++ src/shared/ui/redesigned/Text/Text.tsx
@@ -17,6 +17,7 @@ interface TextProps {
     variant?: TextVariant;
     align?: TextAlign;
     size?: TextSize;
+    bold?: boolean;
 
     'data-testid'?: string;
 }
@@ -43,6 +44,7 @@ export const Text = memo((props: TextProps) => {
         variant = 'primary',
         align = 'left',
         size = 'm',
+        bold,
         'data-testid': dataTestId = 'Text',
     } = props;
 
@@ -52,7 +54,13 @@ export const Text = memo((props: TextProps) => {
     const additionalClasses = [className, cls[variant], cls[align], sizeClass];
 
     return (
-        <div className={classNames(cls.Text, {}, additionalClasses)}>
+        <div
+            className={classNames(
+                cls.Text,
+                { [cls.bold]: bold },
+                additionalClasses,
+            )}
+        >
             {title && (
                 <HeaderTag
                     className={cls.title}
